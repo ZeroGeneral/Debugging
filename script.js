ec365897dc387e862762ec23cfee87a83fe446f6
ZeroGeneral <169157020+ZeroGeneral@users.noreply.github.com>
2024-06-08 20:58:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function myOver(){
+    document.getElementById("over").style.color="blue";
+}
+function myOut(){
+    document.getElementById("over").style.color="white";
+}
+function clickMe(){
+      location.href="other.html";
+}
